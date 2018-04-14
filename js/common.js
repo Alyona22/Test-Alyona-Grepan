612e0396a65a7f93f868f28fc29d7c79905ef29b
Alyona22 <a.grepan@gmail.com>
2018-04-15 00:08:11 +0300

    Change js and add css animation

--- js/common.js
+++ js/common.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 				$(".new-comment__message").css("border", "1px red solid");
 			} else {
 				$("<div class='comments'><h3 class='comments__title'> "+ author +" <span class='comments__date'>" + dateNow + "</span></h3><p class='comments__text'><span class='comments__triangle'></span>" + comment + "</p></div>").appendTo(".comments-wrap");
-				$(".new-comment__message").css("border", "1px solid #a9a9a9");
+				$(".new-comment__message").css("border", "1px solid #a9a9a9").val("");
 			}}
 		});
 		$(".new-comment__btn").click(function(){
@@ -22,7 +22,7 @@ $(document).ready(function(){
 				$(".new-comment__message").css("border", "1px red solid");
 			} else {
 				$("<div class='comments'><h3 class='comments__title'>"+ author +"<span class='comments__date'>" + dateNow + "</span></h3><p class='comments__text'><span class='comments__triangle'></span>" + comment + "</p></div>").appendTo(".comments-wrap");
-				$(".new-comment__message").css("border", "1px solid #a9a9a9");
+				$(".new-comment__message").css("border", "1px solid #a9a9a9").val("");
 			}
 		});
 	});
